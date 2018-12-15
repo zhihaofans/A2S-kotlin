941aadd87f246d9e26e78ac59158eecc162fba7c
zhihaofans <zhihaofans@hotmail.com>
2018-12-15 22:01:31 +0800

    Update

--- app/src/main/java/zhihao/fans/a2s4kotlin/MainActivity.kt
+++ app/src/main/java/zhihao/fans/a2s4kotlin/MainActivity.kt
@@ -1,15 +1,22 @@
 package zhihao.fans.a2s4kotlin
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import com.qmuiteam.qmui.widget.dialog.QMUIDialog
-
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.ViewGroup
 import android.widget.Toast
+import com.qmuiteam.qmui.util.QMUIDisplayHelper
+import com.qmuiteam.qmui.widget.dialog.QMUIDialog
 import com.qmuiteam.qmui.widget.dialog.QMUIDialogAction
+import com.qmuiteam.qmui.widget.grouplist.QMUICommonListItemView
+import com.qmuiteam.qmui.widget.grouplist.QMUIGroupListView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
+        this.init()
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action") {
@@ -27,7 +34,7 @@ class MainActivity : AppCompatActivity() {
                         .setTitle("标题")
                         .setMessage("确定要发送吗？")
                         .addAction("取消") { dialog, index -> dialog.dismiss() }
-                        .addAction(0,"你好", QMUIDialogAction.ACTION_PROP_NEGATIVE) { dialog, index ->
+                        .addAction(0, "你好", QMUIDialogAction.ACTION_PROP_NEGATIVE) { dialog, index ->
                             Toast.makeText(this, "你好", Toast.LENGTH_SHORT).show()
                         }
                         .addAction("确定") { dialog, index ->
@@ -55,4 +62,32 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    private fun init() {
+        val size = QMUIDisplayHelper.dp2px(this, 20)
+        QMUIGroupListView.newSection(this)
+            .setTitle("Section 1: 默认提供的样式")
+            .setDescription("Section 1 的描述")
+            .setLeftIconSize(size, ViewGroup.LayoutParams.WRAP_CONTENT)
+            .addItemView(getQmuiCommonListItemView(this, groupListView, "1", "5", R.mipmap.ic_launcher)) { toast("hi") }
+            .addItemView(getQmuiCommonListItemView(this, groupListView, "2", "4", R.mipmap.ic_launcher)) { toast("hi") }
+            .addItemView(getQmuiCommonListItemView(this, groupListView, "3", "3", R.mipmap.ic_launcher)) { toast("hi") }
+            .addItemView(getQmuiCommonListItemView(this, groupListView, "4", "2", R.mipmap.ic_launcher)) { toast("hi") }
+            .addItemView(getQmuiCommonListItemView(this, groupListView, "5", "1", R.mipmap.ic_launcher)) { toast("hi") }
+            .addTo(groupListView)
+    }
+
+    private fun getQmuiCommonListItemView(
+        context: Context,
+        mGroupListView: QMUIGroupListView,
+        title: String,
+        detailText: String? = null,
+        icon: Int
+    ): QMUICommonListItemView = mGroupListView.createItemView(
+        ContextCompat.getDrawable(context, icon),
+        title,
+        detailText,
+        QMUICommonListItemView.HORIZONTAL,
+        QMUICommonListItemView.ACCESSORY_TYPE_NONE
+    )
 }
