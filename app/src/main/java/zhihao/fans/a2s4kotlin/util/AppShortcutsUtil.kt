c0d808db57ed39bc1b2d1d88d4246b0c41fa36e9
zhihaofans <zhihaofans@hotmail.com>
2018-12-28 23:42:07 +0800

    Update

--- app/src/main/java/zhihao/fans/a2s4kotlin/util/AppShortcutsUtil.kt
+++ app/src/main/java/zhihao/fans/a2s4kotlin/util/AppShortcutsUtil.kt
@@ -2,9 +2,12 @@ package zhihao.fans.a2s4kotlin.util
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.LauncherApps
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
+import android.graphics.drawable.Drawable
 import android.graphics.drawable.Icon
+import android.util.DisplayMetrics
 
 /**
 
@@ -15,6 +18,12 @@ import android.graphics.drawable.Icon
  */
 class AppShortcutsUtil(context: Context) {
     private val mContext = context
+    fun getAppShortcuts() {
+        val shortcutManager = mContext.getSystemService<ShortcutManager>(ShortcutManager::class.java)
+        shortcutManager.dynamicShortcuts.map {
+
+        }
+    }
     fun addShortcut(id: String, shortLabel: String, longLabel: String, icon: Icon, intent: Intent): Boolean {
         val shortcut = ShortcutInfo.Builder(mContext, id)
             .setShortLabel(shortLabel)
@@ -39,4 +48,21 @@ class AppShortcutsUtil(context: Context) {
     fun removeAll() {
         mContext.getSystemService<ShortcutManager>(ShortcutManager::class.java).removeAllDynamicShortcuts()
     }
+
+    fun getAppShortcutIcon(shortcutInfo: ShortcutInfo): Drawable? {
+        val launcherApps = mContext.getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
+        return try {
+            launcherApps.getShortcutIconDrawable(shortcutInfo, DisplayMetrics.DENSITY_DEFAULT)
+        } catch (e: IllegalStateException) {
+            null
+        }
+    }
 }
+
+data class AppShortcutItem(
+    val id: String,
+    val shortLabel: String,
+    val longLabel: String,
+    val icon: Drawable,
+    val intent: Intent
+)
